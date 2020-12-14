55b34820cf1e4286176db919976f07b32378ed49
chlyoo <changhyunlyoo@gmail.com>
2020-12-14 22:09:27 +0900

    webdriver option modified

--- rssvisit.py
+++ rssvisit.py
@@ -30,9 +30,12 @@ def get_rss(url):
 
 def set_driver():
     options = webdriver.ChromeOptions()
+     options.add_argument("start-maximized")
+    options.add_argument("lang=ko_KR")
     options.add_argument('headless')
-    options.add_argument("window-size=1400,600")
+    options.add_argument('window-size=1920x1080')
     options.add_argument("disable-gpu")
+    options.add_argument("--no-sandbox")
     ua = UserAgent()
     a = ua.random
     user_agent = ua.random
