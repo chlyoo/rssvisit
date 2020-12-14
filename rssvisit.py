4e057129247aca2949879d8c04b73990a066160a
chlyoo <changhyunlyoo@gmail.com>
2020-12-14 21:19:27 +0900

    update

---
+++ rssvisit.py
@@ -0,0 +1,52 @@
+from selenium import webdriver
+import requests
+from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
+import random
+import time
+
+
+def get_rss(url):
+    article_list = []
+    try:
+        r = requests.get(url)
+        soup = BeautifulSoup(r.content, features='xml')
+        articles = soup.findAll('item')        
+        for a in articles:
+            title = a.find('title').text
+            link = a.find('link').text
+            published = a.find('pubDate').text
+            article = {
+                'title': title,
+                'link': link,
+                'published': published
+                }
+            article_list.append(article)
+        return article_list
+    except Exception as e:
+        print('The scraping job failed. See exception: ')
+        print(e)
+
+
+def set_driver():
+    options = webdriver.ChromeOptions()
+    options.add_argument("window-size=1400,600")
+    ua = UserAgent()
+    a = ua.random
+    user_agent = ua.random
+    print(user_agent)
+    options.add_argument(f'user-agent={user_agent}')
+    return webdriver.Chrome(chrome_options=options)
+
+if __name__ == '__main__':
+
+    print('Starting scraping')
+    data = get_rss('https://rss.blog.naver.com/peterscience.xml')
+    # print(data)
+    print('Finished scraping')
+
+    for item in data:
+        wd = set_driver()
+        wd.get(item['link'])
+        time.sleep(random.uniform(1,10) * random.randint(1,5))
+        wd.quit()
